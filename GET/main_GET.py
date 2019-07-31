e2f1a6bd2f4c890cacf95bf39fc8c3e69b02f05c
hank9999 <1825355690@qq.com>
2019-07-31 20:13:17 +0800

    增加超线程，内置循环，无需sh/bat

--- GET/main_GET.py
+++ GET/main_GET.py
@@ -1,21 +1,36 @@
 import random
 import string
-import base64
 import urllib.request
+import threading
 
-user1 = ''.join(random.sample(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 9))
-pass1 = ''.join(random.sample(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 6))
-pass2 = ''.join(random.sample(
-    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v',
-     'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z',
-     'X', 'C', 'V', 'B', 'N', 'M'], 3))
-pass3 = ''.join(random.sample(['!', '@', '#', '$', '%', '^', '&', '*', ',', '.', '/'], 2))
-userpass = str(pass1 + pass2 + pass3)
+times = 0
 
-url = "http://xxxx.com/up.php?"+"user="+user1+"&pass="+userpass
-request1 = urllib.request.urlopen(url, timeout=0.5)
 
-print("USER=" + user1)
-print("PASS=" + userpass)
-print(str(request1.read(), 'utf8'))
-print("-------------------------------------------")
+def start_bom():
+    var = 1
+    while var == 1:
+        global times
+        times = times + 1
+        user1 = ''.join(random.sample(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 9))
+        pass1 = ''.join(random.sample(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 6))
+        pass2 = ''.join(random.sample(
+            ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x',
+             'c', 'v',
+             'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K',
+             'L', 'Z',
+             'X', 'C', 'V', 'B', 'N', 'M'], 3))
+        pass3 = ''.join(random.sample(['!', '@', '#', '$', '%', '^', '&', '*', ',', '.', '/'], 2))
+        userpass = str(pass1 + pass2 + pass3)
+        url = "http://xxxx.com/up.php?" + "user=" + user1 + "&pass=" + userpass
+        request1 = urllib.request.urlopen(url, timeout=2)
+        print("USER=" + user1)
+        print("PASS=" + userpass)
+        print(str(request1.read(), 'utf8'))
+        print("-------------------------------------------")
+
+
+event_obj = threading.Event()
+for i in range(32):  # 简易多线程
+    t = threading.Thread(target=start_bom(), args=(event_obj,))
+    t.setDaemon(True)
+    t.start()

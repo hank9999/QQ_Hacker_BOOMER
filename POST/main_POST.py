f7e875345dc126fa65e608465f3036e467fa06cb
hank9999 <1825355690@qq.com>
2019-07-31 21:12:40 +0800

    增加异常处理

--- POST/main_POST.py
+++ POST/main_POST.py
@@ -9,31 +9,40 @@ times = 0
 def start_bom():
     var = 1
     while var == 1:
-        global times
-        times = times + 1
-        user1 = ''.join(random.sample(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 9))
-        pass1 = ''.join(random.sample(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 6))
-        pass2 = ''.join(random.sample(
-            ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x',
-             'c', 'v',
-             'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K',
-             'L', 'Z',
-             'X', 'C', 'V', 'B', 'N', 'M'], 3))
-        pass3 = ''.join(random.sample(['!', '@', '#', '$', '%', '^', '&', '*', ',', '.', '/'], 2))
-        userpass = str(pass1 + pass2 + pass3)
-        r = requests.post(url='http://xxx.com/up.php',
-                          data={'u': user1, 'p': userpass, 'tijiao': '%E6%8F%90%CD%8F%CD%8F%CD%8F%CD%8F%E4%BA%A4'},
-                          headers={'Conten  t-Type': 'application/x-www-form-urlencoded'},
-                          timeout = 2)
-        print(times)
-        print("USER=" + user1)
-        print("PASS=" + userpass)
-        print(r.text)
-        print("-------------------------------------------")
+        try:
+            global times
+            times = times + 1
+            user1 = ''.join(random.sample(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 9))
+            pass1 = ''.join(random.sample(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 6))
+            pass2 = ''.join(random.sample(
+                ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z',
+                 'x',
+                 'c', 'v',
+                 'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J',
+                 'K',
+                 'L', 'Z',
+                 'X', 'C', 'V', 'B', 'N', 'M'], 3))
+            pass3 = ''.join(random.sample(['!', '@', '#', '$', '%', '^', '&', '*', ',', '.', '/'], 2))
+            userpass = str(pass1 + pass2 + pass3)
+            r = requests.post(url='http://xxx.com/up.php',
+                              data={'u': user1, 'p': userpass, 'tijiao': '%E6%8F%90%CD%8F%CD%8F%CD%8F%CD%8F%E4%BA%A4'},
+                              headers={'Conten  t-Type': 'application/x-www-form-urlencoded'},
+                              timeout=2)
+            print(times)
+            print("USER=" + user1)
+            print("PASS=" + userpass)
+            print(r.text)
+            print("-------------------------------------------")
+        except:
+            print("Error!(Maybe timeout)")
 
 
-event_obj = threading.Event()
-for i in range(32):  # 简易多线程
-    t = threading.Thread(target=start_bom(), args=(event_obj,))
-    t.setDaemon(True)
-    t.start()
+if __name__ == "__main__":
+    event_obj = threading.Event()
+    for i in range(256):  # 简易多线程
+        try:
+            t = threading.Thread(target=start_bom(), args=(event_obj,))
+            t.setDaemon(True)
+            t.start()
+        except:
+            print("Thread Create Error!")
